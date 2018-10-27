3af57244305cdb762eb592c1b43a47218ab86e10
wutao123456 <wutao56789@gmail.com>
2018-10-27 15:42:07 +0800

    springboot-config

--- springboot-yaml/src/main/java/com/dlh/yaml/test/TestController.java
+++ springboot-yaml/src/main/java/com/dlh/yaml/test/TestController.java
@@ -1,6 +1,8 @@
 package com.dlh.yaml.test;
 
+import com.dlh.yaml.config.TestBean;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
+    /**
+     * 通过注解将属性直接映射
+     */
     @Value("${com.dlh.name}")
     private String name;
 
@@ -35,5 +40,14 @@ public class TestController {
     public String test1(){
         System.err.println(project +": " + packages);
         return "test1";
+    }
+
+    @Autowired
+    private TestBean testBean;
+
+    @RequestMapping(value = "testbean")
+    public String test2(){
+        System.err.println(testBean.getProject()+": "+testBean.getPackages());
+        return "testbean";
     }
 }
