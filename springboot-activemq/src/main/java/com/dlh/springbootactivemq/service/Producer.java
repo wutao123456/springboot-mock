421be0a1cf33b98b673372b36a5b9b019a735661
wutao123456 <wutao56789@gmail.com>
2018-11-05 23:19:19 +0800

    remove wrong

--- springboot-activemq/src/main/java/com/dlh/springbootactivemq/service/Producer.java
+++
@@ -1,9 +0,0 @@
-package com.dlh.springbootactivemq.service;
-
-/**
- * @author wutao
- * @email wutao56789@gmail.com
- * @date 2018/11/1 23:45
- */
-public class Producer {
-}
--- springboot-websocket/src/main/java/com/dlh/websocket/demo/MyHandler.java
+++
@@ -1,9 +0,0 @@
-package com.dlh.websocket.demo;
-
-/**
- * @author wutao
- * @email wutao56789@gmail.com
- * @date 2018/10/28 13:39
- */
-public class MyHandler {
-}
